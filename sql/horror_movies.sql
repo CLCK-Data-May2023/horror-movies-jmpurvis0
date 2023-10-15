6e3cbd018cfe820d039b55db3adf1610b1fa997f
jmpurvis0 <jmpurvis0@gmail.com>
2023-10-15 17:09:03 -0400

    added SQL query

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,5 @@
--- Add your SQL here
+SELECT m.id AS Movie_ID, m.name AS Movie_Title, m.imdb_rating AS Rating FROM (
+    SELECT id, name, imdb_rating, year, genre FROM movies
+    WHERE year <= 1985 AND genre = "horror"
+    ORDER BY imdb_rating DESC) m
+LIMIT 3
